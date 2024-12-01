54207eea057968ccbb37849059d7044609cd9683
engrsakib <snazmussakib01@gmail.com>
2024-12-01 14:02:15 +0600

    server added

--- index.js
+++ index.js
@@ -90,6 +90,20 @@ async function run() {
       res.send(result);
     })
 
+    // get all users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/users/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
   } finally {
     // Ensures that the client will close when you finish/error
